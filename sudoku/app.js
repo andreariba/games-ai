dd4c846d8b80c96ea646152ea667e2d4c10c709a
Riba <andrea.riba@capgemini.com>
2022-09-16 16:20:46 +0200

    fix first sudoku island solver

--- sudoku/app.js
+++ sudoku/app.js
@@ -39,6 +39,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let width = height = 9;
     let arraySize = width*height;
+    let solveButton = document.getElementById('solve-button')
     let resetButton = document.getElementById('reset-button');
     let grid = document.querySelector('.grid');
     let squares;
@@ -158,54 +159,57 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
 
-    function findIsland(combo) {
+    function keepIslands(combo, listEntries, label="") {
+
+        let index;
+        const size = combo.length;
+        // combo = [...combo];
+        let newEntries = [...listEntries];
+        // console.log(newEntries);
+
+        if(size===0) return newEntries;
     
         // Column
-        let colCount = 0;
+        let Count = 0;
         let totalCount = 0;
-        for (let n=0;n<height;n++) {
-            let count = 0;
-            // console.log(n, col, availableEntries[n*width+col]);
+        for (let n=0;n<listEntries.length;n++) {
+            let localCount = 0;
+            // console.log(listEntries[n]);
             for (let value of combo) {
-                index = availableEntries[n*width+col].indexOf(value);
-                if (index > -1) count++;
+                index = listEntries[n].indexOf(value);
+                if (index > -1) localCount++;
             }
-            totalCount += count;
-            if (count===size) colCount++;
+            totalCount += localCount;
+            if (localCount===size) Count++;
         }
-        // console.log(combo, colCount, totalCount);
+        // console.log(combo, Count, totalCount);
 
-        if (colCount*size===totalCount && colCount===size) {
-            console.log("col island", row, col, combo);
-            for (let n=0;n<height;n++) {
-                // console.log(combo);
+        if (Count*size===totalCount && Count===size) {
+            console.log(label, "island", combo);
+            for (let n=0;n<newEntries.length;n++) {
                 let count = 0;
-                // console.log(n, col, combo, availableEntries[n*width+col]);
-                for (let x of combo) {
-                    console.log(x)
-                }
                 for (let value of combo) {
-                    index = availableEntries[n*width+col].indexOf(value);
-                    // console.log(value, index);
+                    index = newEntries[n].indexOf(value);
                     if (index > -1) count++;
                 }
-                // console.log(n, count);
                 if (count===size) {
-                    availableEntries[n*width+col] = combo;
+                    newEntries[n] = [...combo];
+                    // console.log(n, newEntries[n]);
                 } else {
                     for (let value of combo) {
-                        index = availableEntries[n*width+col].indexOf(value);
-                        if (index > -1) availableEntries[n*width+col].splice(index,1);
+                        index = newEntries[n].indexOf(value);
+                        // console.log(newEntries[n], value, index);
+                        if (index > -1) newEntries[n].splice(index,1);
                     }
                 }
-            }
- 
+            } 
         }
     
+        return newEntries;
     }
 
     
-    function findIsland_old(availableEntries, row, col) {
+    function findIslandsAt(availableEntries, row, col) {
 
         let pos = row*width+col;
         let possibleChoices = [...availableEntries[pos]];
@@ -213,146 +217,51 @@ document.addEventListener('DOMContentLoaded', () => {
 
         let allCombinations = combinations(possibleChoices);
 
-        // console.log(allCombinations)
-        // console.log(allCombinations.length)
-
-        // for (let combo of allCombinations) {
-        //     console.log(combo)
-        // }
-        // return;
         for (let combo of allCombinations) {
-            console.log(combo)
+            // console.log(combo)
 
-            const size = combo.length;
-            if(size===0) continue;
-
-            let index;
-
-            let listColEntries = []
-            for (let n=0;n<height;n++) {
-                listColEntries = [...listColEntries, ...availableEntries[n*width+col]]
-            }
-            
-            return
             // Column
-            let colCount = 0;
-            let totalCount = 0;
+            const listColEntries = []
             for (let n=0;n<height;n++) {
-                let count = 0;
-                // console.log(n, col, availableEntries[n*width+col]);
-                for (let value of combo) {
-                    index = availableEntries[n*width+col].indexOf(value);
-                    if (index > -1) count++;
-                }
-                totalCount += count;
-                if (count===size) colCount++;
+                listColEntries.push([...availableEntries[n*width+col]]);
             }
-            // console.log(combo, colCount, totalCount);
-
-            if (colCount*size===totalCount && colCount===size) {
-                console.log("col island", row, col, combo);
-                for (let n=0;n<height;n++) {
-                    // console.log(combo);
-                    let count = 0;
-                    // console.log(n, col, combo, availableEntries[n*width+col]);
-                    for (let x of combo) {
-                        console.log(x)
-                    }
-                    for (let value of combo) {
-                        index = availableEntries[n*width+col].indexOf(value);
-                        // console.log(value, index);
-                        if (index > -1) count++;
-                    }
-                    // console.log(n, count);
-                    if (count===size) {
-                        availableEntries[n*width+col] = combo;
-                    } else {
-                        for (let value of combo) {
-                            index = availableEntries[n*width+col].indexOf(value);
-                            if (index > -1) availableEntries[n*width+col].splice(index,1);
-                        }
-                    }
-                }
-     
+            // console.log(listColEntries);
+            const newColEntries = keepIslands([...combo], listColEntries, row+","+col+" col");
+            // console.log(newColEntries);
+            for (let n=0;n<height;n++) {
+                // if (n*width+col === 78) console.log("78 ->",newColEntries[n]);
+                availableEntries[n*width+col] = newColEntries[n];
             }
 
             // Row
-            let rowCount = 0;
-            totalCount = 0;
+            const listRowEntries = []
             for (let n=0;n<width;n++) {
-                let count = 0;
-                // console.log(n, col, availableEntries[n*width+col]);
-                for (let value of combo) {
-                    index = availableEntries[row*width+n].indexOf(value);
-                    if (index > -1) count++;
-                }
-                totalCount += count;
-                if (count===size) rowCount++;
+                listRowEntries.push([...availableEntries[row*width+n]])
             }
-            // console.log(combo, rowCount, totalCount);
-
-            if (rowCount*size===totalCount && rowCount===size) {
-                console.log("row island", row, col, combo);
-                for (let n=0;n<width;n++) {
-                    let count = 0;
-                    // console.log(n, col, availableEntries[n*width+col]);
-                    for (let value of combo) {
-                        index = availableEntries[row*width+n].indexOf(value);
-                        if (index > -1) count++;
-                    }
-                    // console.log(n, count);
-                    if (count===size) {
-                        availableEntries[row*width+n] = combo;
-                    } else {
-                        for (let value of combo) {
-                            index = availableEntries[row*width+n].indexOf(value);
-                            if (index > -1) availableEntries[row*width+n].splice(index,1);
-                        }
-                    }
-                }
-     
+            // console.log(listRowEntries);
+            const newRowEntries = keepIslands(combo, listRowEntries, row+","+col+" row");
+            // console.log(newRowEntries);
+            for (let n=0;n<width;n++) {
+                // if (row*width+n === 78) console.log("78 ->", newRowEntries[n]);
+                availableEntries[row*width+n] = newRowEntries[n];
             }
 
-
-            // Quadrant
-            quadrant_row = Math.floor(row/3)*3 + 1;
-            quadrant_col = Math.floor(col/3)*3 + 1;
-            
-            let squareCount = 0;
-            totalCount = 0;
+            // Quadrant 
+            quadrant_row = Math.floor(row/3)*3+1;
+            quadrant_col = Math.floor(col/3)*3+1;
+            const listQuadrantEntries = []
             for (let i=-1;i<=1;i++) {
                 for (let j=-1;j<=1;j++) {
-                    let count = 0;
-                    for (let value of combo) {
-                        index = availableEntries[(quadrant_row+i)*width + quadrant_col+j].indexOf(value);
-                        if (index > -1) count++;
-                    }
-                    totalCount += count;
-                    if (count===size) squareCount++;
+                    listQuadrantEntries.push([...availableEntries[(quadrant_row+i)*width + quadrant_col+j]])
                 }
             }
-
-            // console.log(combo, squareCount, totalCount);
-            
-            if (squareCount*size===totalCount && squareCount===size) {
-                console.log("square island", row, col, combo);
-                for (let i=-1;i<=1;i++) {
-                    for (let j=-1;j<=1;j++) {
-                        let count = 0;
-                        for (let value of combo) {
-                            index = availableEntries[(quadrant_row+i)*width + quadrant_col+j].indexOf(value);
-                            if (index > -1) count++;
-                        }
-                        // console.log(n, count);
-                        if (count===size) {
-                            availableEntries[(quadrant_row+i)*width + quadrant_col+j] = combo;
-                        } else {
-                            for (let value of combo) {
-                                index = availableEntries[(quadrant_row+i)*width + quadrant_col+j].indexOf(value);
-                                if (index > -1) availableEntries[(quadrant_row+i)*width + quadrant_col+j].splice(index,1);
-                            }
-                        }
-                    }
+            // console.log(listQuadrantEntries);
+            const newQuadrantEntries = keepIslands([...combo], listQuadrantEntries, row+","+col+" quad");
+            // console.log(newQuadrantEntries);
+            for (let i=-1;i<=1;i++) {
+                for (let j=-1;j<=1;j++) {
+                    // if ((quadrant_row+i)*width + quadrant_col+j === 78) console.log("78 ->",newQuadrantEntries[(i+1)*3+j+1]);
+                    availableEntries[(quadrant_row+i)*width + quadrant_col+j] = newQuadrantEntries[(i+1)*3+j+1];
                 }
             }
         }
@@ -365,69 +274,95 @@ document.addEventListener('DOMContentLoaded', () => {
         for (let r=0;r<height;r++) {
             for (let c=0;c<width;c++) {
                 let available = checkAvailableEntries(grid, r, c);
-                availableEntries[r*width+c] = available;
+                if (available.length===1) {
+                    grid[r][c] = available[0];
+                    availableEntries[r*width+c] = [];
+                    init_available(grid);
+                } else {
+                    availableEntries[r*width+c] = available;
+                }
             }
         }
 
         return availableEntries
     };
 
+    async function solve(grid, availableEntries = {}) {
 
-    function solve(grid, availableEntries = {}) {
+        console.log("SOLVE");
 
-        for (let r=0;r<height;r++) {
-            for (let c=0;c<width;c++) {
-                let available = availableEntries[r*width +c];
-                if (available.length===1) {
-                    grid[r][c] = available[0];
-                    availableEntries[r*width +c].pop();
-                    //solve(grid, availableEntries);
-                }
-            }
-        }
-    
-        // more complex cases
         for (let pos in availableEntries) {
     
             intPos = parseInt(pos);
             row = Math.floor(intPos/width);
             col = intPos % width;
-    
-            // console.log(pos, intPos, row, col, availableEntries[pos]);
-            // findIsland(availableEntries, row, col);
-
-            for (let value of availableEntries[pos]) {
-    
-                // console.log(row, col, value);
-    
-                if ( onlyPossible(availableEntries, row, col, value) ) {
-                    grid[row][col] = value;
-                    const indexToDelete = availableEntries[row*width +col].indexOf(value);
-                    availableEntries[row*width +col].splice(indexToDelete,1);
-                    solve(grid, availableEntries);
-                }
-                
-            }
+            // console.log("Checking", pos, row, col);
+            findIslandsAt(availableEntries, row, col);
+            // console.log("ONLY", 7*width+1,availableEntries[7*width+1])
         }
 
-        // if (grid.filter(v => v === 0).length===0) {}
+        // console.log("OUT", 7*width+1,availableEntries[7*width+1])
+        drawGrid(grid, availableEntries);
+        // console.log("OUT", 7*width+1,availableEntries[7*width+1])
+
+        console.log("UPGRADE");
+
+        for (let r=0;r<height;r++) {
+            for (let c=0;c<width;c++) {
+                const available = availableEntries[r*width +c];
+                // console.log("Upgrading", r,c, available, available.length);
+
+                if (available.length===0 && grid[r][c]===0) {
+                    console.log("ERROR", r*width+c, r, c, grid[r][c], available);
+                    return;
+                } else if (available.length===1) {
+                    let value = available.pop();
+                    
+                    console.log("Update",r, c,"to",value,"remaining",availableEntries[r*width +c]);
+
+                    grid[r][c] = value;
+
+                    let index;
+
+                    for (let n=0;n<height;n++) {
+                        index = availableEntries[n*width+c].indexOf(value);
+                        if (index > -1) availableEntries[n*width+c].splice(index,1);
+                    }
+            
+                    for (let n=0;n<width;n++) {
+                        index = availableEntries[r*width+n].indexOf(value);
+                        if (index > -1) availableEntries[r*width+n].splice(index,1);
+                    }
+                
+                    quadrant_row = Math.floor(r/3)*3 + 1;
+                    quadrant_col = Math.floor(c/3)*3 + 1;
+                    for (let i=-1;i<=1;i++) {
+                        for (let j=-1;j<=1;j++) {
+                            index = availableEntries[(quadrant_row+i)*width + quadrant_col+j].indexOf(value);
+                            if (index > -1) availableEntries[(quadrant_row+i)*width + quadrant_col+j].splice(index,1);
+                        }
+                    }
+
+                }
+
+
+            }
+        }
+        // drawGrid(grid, availableEntries);
+        await new Promise(r => setTimeout(r, 2000));
+
+        const number_of_zeros = grid.flat().filter(v => v === 0).length;
+        // console.log(grid, number_of_zeros);
+        // if (number_of_zeros!==0) {
         //     solve(grid, availableEntries);
+        // }
     
-        // return grid;
     };
 
-    availableEntries = init_available(testEasy);
-    solve(testEasy, availableEntries);
-    drawGrid(testEasy, availableEntries);
-    findIsland(availableEntries, 1, 7);
-    findIsland(availableEntries, 1, 2);
-    drawGrid(testEasy, availableEntries);
+    solveButton.addEventListener('click', () => { solve(grid, availableEntries); drawGrid(grid, availableEntries);});
 
-    // availableEntries = init_available(testMedium);
-    // solve(testMedium, availableEntries);
-    // drawGrid(testMedium, availableEntries);
+    grid = testMedium;
+    availableEntries = init_available(grid);
+    drawGrid(grid, availableEntries);
 
-    // availableEntries = init_available(testHard);
-    // solve(testHard, availableEntries);
-    // drawGrid(testHard, availableEntries);
 })
